6e8cf9bb6f4f98cfa533593069585806a9165432
newtondotcom <asphalt9fr@gmail.com>
2025-03-13 15:20:08 +0100

    feat: create signin page

---
+++ app/auth/signin/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { signIn } from "next-auth/react";
+
+export default function SignIn() {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-3xl font-bold mb-6">
+        Pour accéder à la <span className="text-primary">plateforme</span>,
+        veuillez vous connecter.
+      </h1>
+      <div className="container flex flex-col items-center py-20">
+        <Button
+          variant="secondary"
+          onClick={() => signIn("authentik", { callbackUrl: "/dashboard" })}
+        >
+          <img src="https://git.inpt.fr/inp-net/visual-identity/-/raw/main/derivations/auth.svg" />
+          Connexion avec INP-net
+        </Button>
+      </div>
+    </div>
+  );
+}
